bab9783a2c5a330aa67246f45275d5eb9fed943b
Arkadi Listov <arkalis@greeny.cs.tlu.ee>
2016-02-10 10:49:29 +0200

    algus

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-as
+(function(){
+   "use strict";
+
+
+    var Moosipurk = function(){
+
+      // singelton pattern (4rida)
+      if(Moosipurk.instance){
+        return Moosipurk.instance;
+      }
+      Moosipurk.instance = this; //this viitab moosipurgile
+
+      console.log('moosipurgi sees');
+    };
+
+    window.onload = function(){
+      var app = new Moosipurk();
+    };
+
+})();
